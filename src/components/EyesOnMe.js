a46e2553038fb466cd21d8b40b44d9f3b7888361
zacharytq <zacharytq@gmail.com>
2021-02-21 22:21:20 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+    constructor() {
+        super()
+    }
+
+    fireGood = () => {
+        console.log('Good!')
+    }
+
+    fireFocus = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return(
+            <button onBlur={this.fireFocus} onFocus={this.fireGood} />
+        )
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React, { Component } from 'react';
+
+export default class Keypad extends Component {
+    constructor() {
+        super()
+    }
+
+    popUp = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return(
+        <input onKeyUp={this.popUp} type='password'/>
+        )
+    }
+}
